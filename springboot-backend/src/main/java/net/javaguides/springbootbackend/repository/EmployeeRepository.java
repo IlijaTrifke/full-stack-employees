4a2998a821d95607103a54dbe51cb7a1a45304ab
IlijaTrifke <gtrifunovic05@gmail.com>
2024-02-09 13:39:32 +0100

    Refactor code for improved performance

---
+++ springboot-backend/src/main/java/net/javaguides/springbootbackend/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package net.javaguides.springbootbackend.repository;
+
+import net.javaguides.springbootbackend.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
